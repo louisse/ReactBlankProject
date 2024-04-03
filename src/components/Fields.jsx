274d522a50b8c4434cc9f8ea2fc2315a5adb90c3
Roger Louisse Eustaquio <louisseeustaquio@gmail.com>
2024-04-04 06:24:48 +0800

    Implement hide-show password

--- src/components/Fields.jsx
+++ src/components/Fields.jsx
@@ -1,7 +1,10 @@
 import { Inputs } from "../constants/input";
 import ErrorBox from "../components/error.jsx";
+import { EyeIcon } from "../icons/icons.jsx";
+import { useRef } from "react";
 
 export default function Fields({ formState, setFormState }) {
+  const passwordRef = useRef(null);
   const handleChange = (value, inputValue) => {
     setFormState((prevState) => ({
       ...prevState,
@@ -11,19 +14,34 @@ export default function Fields({ formState, setFormState }) {
       },
     }));
   };
-
+  const handleHideShowPassword = () => {
+    if (passwordRef.current.type === "password") {
+      passwordRef.current.type = "text";
+    } else {
+      passwordRef.current.type = "password";
+    }
+  };
   return Inputs.map((input) => (
     <div className="form-field" key={input.value}>
       <label className="label">{input.label}</label>
-      <input
-        type={input.type}
-        placeholder={input.placeholder}
-        onChange={(e) => handleChange(input.value, e.target.value)}
-        value={formState[input.value].value}
-        className={
-          !!formState[input.value].error ? "input error-field" : "input"
-        }
-      />
+      <div className="relative">
+        <input
+          type={input.type}
+          placeholder={input.placeholder}
+          onChange={(e) => handleChange(input.value, e.target.value)}
+          value={formState[input.value].value}
+          className={
+            "input " + (!!formState[input.value].error ? "error-field" : "")
+          }
+          ref={input.value === "password" ? passwordRef : null}
+        />
+        {input.value === "password" ? (
+          <div className="eye-icon-wrapper" onClick={handleHideShowPassword}>
+            <EyeIcon />
+          </div>
+        ) : null}
+      </div>
+
       <ErrorBox message={formState[input.value].error} />
     </div>
   ));
